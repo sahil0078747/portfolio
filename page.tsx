362f6131bb7e8b9d038f70771a47efea255c06b5
sahil0078747 <sahilog007@gmail.com>
2025-07-21 20:00:59 +0530

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,23 @@
+import Hero from "./components/Hero"
+import About from "./components/About"
+import Projects from "./components/Projects"
+import Skills from "./components/Skills"
+import Services from "./components/Services"
+import FreelanceProfile from "./components/FreelanceProfile"
+import FAQ from "./components/FAQ"
+import Contact from "./components/Contact"
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Projects />
+      <Skills />
+      <Services />
+      <FreelanceProfile />
+      <FAQ />
+      <Contact />
+    </>
+  )
+}
